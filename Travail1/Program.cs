5861756edf542a52b3d9d673b125a6de7f8d5606
AntonyCollin <72162477+AntonyCollin@users.noreply.github.com>
2023-03-23 13:01:41 -0400

    Update Program.cs

--- Travail1/Program.cs
+++ Travail1/Program.cs
@@ -9,7 +9,7 @@ namespace Travail1
     internal static class Program
     {
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        /// Point d'entrée principal de l'application..
         /// </summary>
         [STAThread]
         static void Main()
